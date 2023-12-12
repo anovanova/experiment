4f4c2afcde2af56cc12c4207896bf795f64bc2cd
macedera <michael.acedera@fasttrackph.com>
2023-12-12 16:27:06 +0800

    initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+import { ref } from 'vue';
+
+const date = ref(new Date());
+const format = (date: { getDate: () => any; getMonth: () => number; getFullYear: () => any; }) => {
+  const day = date.getDate();
+  const month = date.getMonth() + 1;
+  const year = date.getFullYear();
+
+  return `${day}/${month}/${year}`;
+}
+</script>
+
+<template>
+  <main>
+    <h1>PEPSI IS MY DRINK</h1>
+    <VueDatePicker v-model="date" :format="format"/>
+    <input type="date" name="" id="">
+    <TheWelcome />
+  </main>
+</template>
